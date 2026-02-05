74275776f486ea0bfe53efff7ac43133d82ea177
t-noda-211 <takeru.940531@gmail.com>
2026-02-05 21:00:21 +0900

    パスの設定変更

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -18,6 +18,7 @@ export default {
   },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
+    '^shared/(.*)$': '<rootDir>/../shared/src/$1',
   },
   collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts'],
 }
--- shared/src/index.ts
+++
@@ -1,1 +0,0 @@
-export { AppDateTime } from './appDateTime'
